977683c5f0497a2aa1c6dede59e29a4d2da4be86
velezmolina <velezmolina@gmail.com>
2024-08-18 16:05:09 +0000

    Getting data from firebase

--- lib/auth.dart
+++ lib/auth.dart
@@ -3,7 +3,7 @@ import 'package:google_sign_in/google_sign_in.dart';
 import 'package:firebase_auth/firebase_auth.dart' as auth;
 
 class Auth extends StatelessWidget {
-  Auth({ Key? key }) : super(key: key);
+  Auth({Key? key}) : super(key: key);
 
   final GoogleSignIn _googleSignIn = GoogleSignIn();
   final auth.FirebaseAuth _auth = auth.FirebaseAuth.instance;
@@ -14,12 +14,13 @@ class Auth extends StatelessWidget {
       body: Center(
         child: ElevatedButton(
           onPressed: googleSignIn,
-          child: const Text('Login with Google'),
+          child: const Text('Login with Googles'),
         ),
       ),
     );
   }
-    //Sign in with google auth
+
+  //Sign in with google auth
   Future googleSignIn() async {
     final googleUser = await _googleSignIn.signIn();
     final googleAuth = await googleUser!.authentication;
@@ -28,6 +29,7 @@ class Auth extends StatelessWidget {
       idToken: googleAuth.idToken,
     );
     final user = await _auth.signInWithCredential(credential);
+    //var user = await _auth.signInWithPopup(auth.GoogleAuthProvider());
     return user;
   }
 }
--- lib/home.dart
+++ lib/home.dart
@@ -1,43 +1,144 @@
+import 'dart:async';
 import 'dart:convert';
 
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class AppBars extends AppBar {
+  // Suggested code may be subject to a license. Learn more: ~LicenseLog:1784550236.
+  AppBars(User? userInfo, {super.key})
+      : super(
+          title: const Center(child: Text("ServControl")),
+          actions: <Widget>[
+// Suggested code may be subject to a license. Learn more: ~LicenseLog:976259272.
+            CircleAvatar(
+              backgroundImage: NetworkImage(userInfo!.photoURL.toString()),
+            ),
+            /* IconButton(
+            //icon: const Icon(Icons.comment),
+// Suggested code may be subject to a license. Learn more: ~LicenseLog:1098745684.
+            icon: const Icon(Icons.comment),
+            tooltip: 'Comment Icon',
+            onPressed: () {},
+          ), //IconButton
+          IconButton(
+            icon: const Icon(Icons.settings),
+            tooltip: 'Setting Icon',
+            onPressed: () {},
+          ), //IconButton */
+          ], //<Widget>[]
+          backgroundColor: Colors.white,
+          elevation: 50.0,
+          leading: IconButton(
+            icon: const Icon(Icons.menu),
+            tooltip: 'Menu Icon',
+            onPressed: () {},
+          ),
+          systemOverlayStyle: SystemUiOverlayStyle.light,
+        );
+}
+
+class DataTableExample extends StatelessWidget {
+  final Map<String, dynamic> srvdata;
+  const DataTableExample({super.key, required this.srvdata});
+
+  @override
+  Widget build(BuildContext context) {
+    print('DATABALE');
+    for (var value in srvdata.values) {
+      print(value['est']);
+    }
+
+    return DataTable(
+      columns: const <DataColumn>[
+        DataColumn(
+          label: Expanded(
+            child: Text(
+              'Date',
+              style: TextStyle(fontStyle: FontStyle.italic),
+            ),
+          ),
+        ),
+        DataColumn(
+          label: Expanded(
+            child: Text(
+              'Hours',
+              style: TextStyle(fontStyle: FontStyle.italic),
+            ),
+          ),
+        ),
+        DataColumn(
+          label: Expanded(
+            child: Text(
+              'Studies',
+              style: TextStyle(fontStyle: FontStyle.italic),
+            ),
+          ),
+        ),
+      ],
+      rows: srvdata.values
+          .map((value) => DataRow(cells: [
+                DataCell(Text(value['date'])),
+                DataCell(Text(value['horas'])),
+                DataCell(Text(value['est'])),
+              ]))
+          .toList(),
+    );
+  }
+}
 
 class HomePage extends StatelessWidget {
-   HomePage({Key? key}) : super(key: key);
+  HomePage({Key? key}) : super(key: key);
   //const HomePage(User? user, {super.key});
   final User? userInfo = FirebaseAuth.instance.currentUser;
+  Map<String, dynamic> currentData = {};
+  //final databaseReference = FirebaseDatabase.instance.ref();
 
   Future<void> readData() async {
     print("READING DATABASE");
-    DatabaseReference databaseReference =
-        FirebaseDatabase.instance.ref().child('users');
+    final now = DateTime.now();
+    print(now.year);
+    DatabaseReference databaseReference = FirebaseDatabase.instance
+        .ref()
+        .child('control/${userInfo!.displayName?.split(" ")[0]}/${now.year}/5');
 
     databaseReference.onValue.listen((event) {
       DataSnapshot dataSnapshot = event.snapshot;
       print(dataSnapshot.value);
       Map<String, dynamic> data = jsonDecode(jsonEncode(dataSnapshot.value));
+      currentData = data;
       //Map<String, dynamic> values = dataSnapshot.value as Map<String, dynamic>;
-      data.forEach((key, values) {
-        print('Key: $key');
-        print('Name: ${values['name']}');
+      for (var value in currentData.values) {
+        //print('Key: $key');
+        print(value['date']);
+        /* print('Name: ${values['name']}');
         print('Email: ${values['email']}');
-        print('Age: ${values['age']}');
-      });
+        print('Age: ${values['age']}'); */
+      }
     });
   }
 
   @override
-  
   Widget build(BuildContext context) {
     readData();
     print('USER = $userInfo');
     return Scaffold(
-      body: Center(
-// Suggested code may be subject to a license. Learn more: ~LicenseLog:580700638.
-        child: Text('Home Page of ServControl ${userInfo?.displayName}'),
-      ),
-    );
+        appBar: AppBars(userInfo),
+        body: Center(
+          child: DataTableExample(srvdata: currentData),
+        ));
+    /* body: Center(
+        child: Text(
+          'Home Page of ServControl ${userInfo!.displayName?.split(" ")[0]}',
+          style: const TextStyle(fontSize: 24),
+        ), //Text 
+      ), //Center
+      //Scaffold
+      // debugShowCheckedModeBanner: false, //Removing Debug Banner
+    );*/
   }
+
+  center(Text text) {}
 }
